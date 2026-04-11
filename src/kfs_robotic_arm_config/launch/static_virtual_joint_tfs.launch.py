cf147345b566eaffbf7a672d73fe20fa56c2db92
vignesh <arivazhaganvignesh612@gmail.com>
2026-04-11 23:17:35 +0530

    add the robotic arm for moveit2

--- src/kfs_robotic_arm_config/launch/static_virtual_joint_tfs.launch.py
+++ src/kfs_robotic_arm_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arm", package_name="kfs_arm_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arm", package_name="kfs_robotic_arm_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
